a67be41b3a0c1befebfb92d6dac66ee9f76a50a2
Anna W <aweinmacher@gmail.com>
2017-12-18 16:48:07 +0200

    individual exercises

--- main.js
+++ main.js
@@ -6,7 +6,22 @@ var reservations = {
 var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
-  // write your code here!
+  name = name.toLowerCase();
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  for (key in reservations) {
+    if (key === name && reservations[name].claimed === false) {
+      alert("Welcome, " + name + "!");
+      return;
+    }
+    if (key === name && reservations[name].claimed === true) {
+      alert("Your reservations has already been claimed, " + name + "!");
+      return;
+    }
+  }
+  reservations[name] = { claimed: true };
+  alert("There's nothing under your name, " + name + ", but we have made it for you!");
+  console.log(reservations);
+  return;
 }
 
 claimReservation();
